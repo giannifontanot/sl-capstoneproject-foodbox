0b84671b9e67c69821cf4f2f5c7c49c7f0f13b5c
Gianni Fontanot <giannifontanot@gmail.com>
2022-08-22 18:26:09 -0500

    Springboot and Angular project, first commit.

---
+++ src/main/java/com/foodbox/SlCapstoneprojectFoodboxApplication.java
@@ -0,0 +1,17 @@
+package com.foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SlCapstoneprojectFoodboxApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlCapstoneprojectFoodboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodbox/SlCapstoneprojectFoodboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlCapstoneprojectFoodboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
